f925fde4c51f594454c81a80b40dd8b765cdead7
Nikita Lutoshkin <n28280202@gmail.com>
2021-02-12 19:00:51 +0300

    lesson 12 ready to check

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,73 @@
 'use strict'
-alert(1);
+
+const todoControl = document.querySelector('.todo-control'),
+	headerInput = document.querySelector('.header-input'),
+	todoList = document.querySelector('.todo-list'),
+	todoCompleted = document.querySelector('.todo-completed');
+	
+	
+	let todoData = [];
+
+	todoData = JSON.parse(localStorage.getItem('data'));
+
+	const render = function () {
+
+	todoList.textContent = '';
+	todoCompleted.textContent = '';
+	
+	todoData.forEach(function(item) {
+	
+		const li = document.createElement('li');
+		li.classList.add('todo-item');
+		li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+		'<div class="todo-buttons">' +
+		'<button class="todo-remove"></button>' +
+		'<button class="todo-complete"></button>' +
+		'</div>';
+
+		if (item.remove) {
+			return;
+		} else if (item.completed) {
+			todoCompleted.append(li);
+		} else {
+			todoList.append(li);
+		};
+
+		const btnTodoCompleted = li.querySelector('.todo-complete');
+
+		btnTodoCompleted.addEventListener('click', function() { 
+			item.completed = !item.completed;
+			render();
+		});
+
+		const btnTodoRemove = li.querySelector('.todo-remove');
+
+		btnTodoRemove.addEventListener('click', function() {
+			item.remove = true;
+			btnTodoRemove.parentNode.parentNode.remove();
+			render();
+		});
+	});
+
+	localStorage.setItem('data', JSON.stringify(todoData));
+};
+
+
+todoControl.addEventListener('submit', function (event) {
+	event.preventDefault();
+
+	if (headerInput.value !== '') {
+		const newTodo = {
+			value: headerInput.value,
+			completed: false,
+		};
+
+		todoData.push(newTodo);
+
+	};
+	headerInput.value = '';
+	
+	render();
+});
+
+render();
